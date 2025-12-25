e635c295007aca77ba9909e2b9f459454d51ba6f
Nathaniel M. Abaya <nathanielabaya88@gmail.com>
2025-12-25 14:34:38 +0800

    Create users.js

---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+export const ADMIN_CREDENTIALS = {
+  email: "admin@cvsu.edu.ph",
+  password: "admin123"
+};
+
+export const STUDENTS = [
+  {
+    name: "John Doe",
+    email: "john@cvsu.edu.ph",
+    studentId: "2025001"
+  },
+  {
+    name: "Paul Smith",
+    email: "paul@cvsu.edu.ph",
+    studentId: "2025002"
+  },
+  {
+    name: "Sarah Jones",
+    email: "sarah@cvsu.edu.ph",
+    studentId: "2025003"
+  }
+];
